e30d8772e52e826c666767b13547c35f0ffe3a0d
MarinPaz7 <99123733+MarinPaz7@users.noreply.github.com>
2022-03-08 22:23:38 +0100

    Initial commit

---
+++ Moodchecker.py
@@ -0,0 +1,11 @@
+ime = input(" Kako se zoveš? :) ")
+mood = input("kako se osjećaš " + ime + " ? ")
+if mood == "super":
+    print("Hej, " + ime + " lijepo te je vdijeti " + mood + " raspoloženja :) ")
+elif mood == "nervozno":
+    print("Duboko udahni i smiri se! Sve će biti u redu :)")
+elif mood == "tužno":
+    print("Hej dijamantu, nemoj biti tužan! Zasjati ćeš u punom sjaju ;)")
+else:
+    print("Idemo osvojiti svijet " + ime + "!! :)")
+
